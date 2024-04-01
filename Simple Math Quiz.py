05cbdb02a8c304dddf70fe1f482f0e0e93cdb8ad
daraudoh <162847659+daraudoh@users.noreply.github.com>
2024-04-01 09:56:54 -0700

    Rearranging the functions in the code to make it more readable and easier to understand.

--- Simple Math Quiz.py
+++ Simple Math Quiz.py
@@ -13,10 +13,18 @@ def random_number (num1, num2):
 num1 = random_number(10, 20)
 num2 = random_number(1, 10)
 
-#defining the random question generator
+#defining the random addition question generator
 def random_question_add():
-    question = input("What is " + str(num1) + " " + "+" + " " + str(num2) + " ?")
+    question = ("What is " + str(num1) + " " + "+" + " " + str(num2) + " ?")
     print (question)
+
+def user_answer_add():
+    user_answer = input("Please type you answer here: ")
+    print (user_answer)
+
+def correct_answer_add(): 
+    correct_answer = "The correct answer is",  num1 + num2
+    print (correct_answer)  
     
 
 def random_question_sub():
@@ -33,15 +41,11 @@ def random_question_div():
 
 
 random_question_add()
+user_answer_add()
+correct_answer_add()
 #correct answer may need own def to get userannswer = correcT answer
 
-def user_answer_add():
-    user_answer = input("What is your answer? ")
-    print (user_answer)
-
-def correct_answer_add(): 
-    correct_answer = "The correct answer is: ", num1 + num2
-    print (correct_answer)    
+ 
 
    
 
