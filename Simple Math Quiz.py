f69e95fe02226c8daeb34448c9cb4d2b8b1798a5
daraudoh <162847659+daraudoh@users.noreply.github.com>
2024-04-02 10:59:42 -0700

    Changed the  code to generate random numbers for every question and also contained user answer and correct answer in the output in the random question function

--- Simple Math Quiz.py
+++ Simple Math Quiz.py
@@ -20,73 +20,104 @@ num2 = random_number(1, 10)
 
 #defining the random addition question generator
 def random_question_add():
+    global score
+    num1 = random_number(10, 20)
+    num2 = random_number(1, 10)
     question = ("What is " + str(num1) + " " + "+" + " " + str(num2) + " ?")
     print (question)
+    correct_answer_add = num1 + num2
+    user_answer_add = int(input("Please type you answer here: "))
+    if correct_answer_add == user_answer_add:
+        score += 1
+        print ("You are correct!")
+    else:    
+        print ("This is incorrect, the correct answer is: " + str(correct_answer_add) + ".")
+
 
 #defining the random subtraction question generator
 def random_question_sub():
+    global score
+    num1 = random_number(10, 20)
+    num2 = random_number(1, 10)
     question = "What is " + str(num1) + " " + "-" + " " + str(num2) + " ?"
     print (question)
-
-#defining the random multiplication question generator
-def random_question_mult():
-    question =  "What is " + str(num1) + " " + "*" + " " + str(num2) + " ?"
-    print (question)
-
-#defining the random division question generator
-def random_question_div():
-    question = "What is " + str(num1) + " " + "/" + " " + str(num2) + " ?"
-    print (question)
-
-#Adding the correct answer variables
-correct_answer_add = num1 + num2   
-correct_answer_sub = num1 - num2
-correct_answer_mult = num1 * num2
-correct_answer_div = num1 / num2
-
-#calling the random question functions
-random_question_add()
-user_answer_add = int(input("Please type you answer here: "))
-
-random_question_sub()
-user_answer_sub = int(input("Please type you answer here: "))
-
-random_question_mult()
-user_answer_mult = int(input("Please type you answer here: "))
-
-random_question_div()
-user_answer_div = int(input("Please type you answer here: "))
-
-random_question_mult()
-user_answer_mult = int(input("Please type you answer here: "))
-
-
-#Showing the correct answers
-while score < 8:
-    if user_answer_add == correct_answer_add:
-        score += 2
-        print ("You are correct!")
-    else:
-        print ("This is incorrect, the correct answer is: " + str(correct_answer_add) + ".")
+    correct_answer_sub = num1 - num2 
+    user_answer_sub = int(input("Please type you answer here: "))
     if user_answer_sub == correct_answer_sub:
-        score += 2
+        score += 1
         print ("You are correct!")
     else:
         print ("This is incorrect, the correct answer is: " + str(correct_answer_sub) + ".")
+    
+
+#defining the random multiplication question generator
+def random_question_mult():
+    global score
+    num1 = random_number(10, 20)
+    num2 = random_number(1, 10)
+    question =  "What is " + str(num1) + " " + "*" + " " + str(num2) + " ?"
+    print (question)
+    correct_answer_mult = num1 * num2
+    user_answer_mult = int(input("Please type you answer here: "))
     if user_answer_mult == correct_answer_mult:
-        score += 2
+        score += 1
         print ("You are correct!")
     else:
         print ("This is incorrect, the correct answer is: " + str(correct_answer_mult) + ".")
+    
+
+#defining the random division question generator
+def random_question_div():
+    global score
+    num1 = random_number(10, 20)
+    num2 = random_number(1, 10)
+    question = "What is " + str(num1) + " " + "/" + " " + str(num2) + " ?"
+    print (question)
+    correct_answer_div = num1 / num2
+    user_answer_div = int(input("Please type you answer here: "))
     if user_answer_div == correct_answer_div:
-        score += 2
+        score += 1
         print ("You are correct!")
     else:
         print ("This is incorrect, the correct answer is: " + str(correct_answer_div) + ".")
-    break
 
+
+#calling the random question functions to total 10 question
+print ()   #creating a space between the questions for better readability      
+random_question_mult()  #1
+print () 
+
+random_question_add()   #2
+print ()
+
+random_question_sub()   #3
+print ()
+
+random_question_mult()  #4
+print ()
+
+random_question_sub()   #5
+print ()
+
+random_question_div()   #6    
+print ()
+
+random_question_mult()  #7
+print ()
+
+random_question_add()   #8
+print ()
+
+random_question_sub()   #9        
+print ()
+
+random_question_div()   #10
+print ()
+
+#showing the user their score
 print ("Your score is: " + str(score))
 
+#determining if the user has passed or failed the quiz
 if score >= 8:
     print ("Congratulations " + username + ", you have passed the quiz!")   
 
--- testingcode.py
+++ testingcode.py
@@ -1,22 +1,65 @@
-num1 = 5
-num2 = 10
+import random
 
+for _ in range(10):  # replace 10 with the number of questions you want
+    num1 = random.randint(1, 10)  # replace 1, 10 with the range of numbers you want
+    num2 = random.randint(1, 10)
+
+    random_question_add = input("What is " + str(num1) + " " + "+" + " " + str(num2) + " ?")
+    random_question_div = input("What is " + str(num1) + " " + "/" + " " + str(num2) + " ?")
+    random_question_mult = input("What is " + str(num1) + " " + "*" + " " + str(num2) + " ?")   
+    random_question_sub = input("What is " + str(num1) + " " + "-" + " " + str(num2) + " ?")    
+    # rest of your code here
+
+#defining the random addition question generator
 def random_question_add():
     question = ("What is " + str(num1) + " " + "+" + " " + str(num2) + " ?")
     print (question)
 
+#defining the random subtraction question generator
+def random_question_sub():
+    question = "What is " + str(num1) + " " + "-" + " " + str(num2) + " ?"
+    print (question)
+
+#defining the random multiplication question generator
+def random_question_mult():
+    question =  "What is " + str(num1) + " " + "*" + " " + str(num2) + " ?"
+    print (question)
+
+#defining the random division question generator
+def random_question_div():
+    question = "What is " + str(num1) + " " + "/" + " " + str(num2) + " ?"
+    print (question)
+
+#Adding the correct answer variables
+correct_answer_add = num1 + num2   
+correct_answer_sub = num1 - num2
+correct_answer_mult = num1 * num2
+correct_answer_div = num1 / num2
+
 random_question_add()
-user_answer = int (input("Please type you answer here: "))
-print (user_answer)
+answer= int(input("Please type you answer here: "))
+random_question_sub()
+answer= int(input("Please type you answer here: "))
+random_question_mult()
+answer= int(input("Please type you answer here: "))
+random_question_div()
+answer= int(input("Please type you answer here: "))
 
-
-correct_answer = num1 + num2
-print ("The correct answer is", correct_answer)
-
-
-if user_answer == correct_answer:
-    print ("you are correct")
-
-else:
-    print ("incorrect")
+if answer == correct_answer_add:
+    print ("You are correct!")
+else: 
+    print ("This is incorrect, the correct answer is: " + str(correct_answer_add) + ".")
+if answer == correct_answer_sub:
+    print ("You are correct!")
+else:    
+    print ("This is incorrect, the correct answer is: " + str(correct_answer_sub) + ".")
+if answer == correct_answer_mult:
+    print ("You are correct!")
+else:    
+    print ("This is incorrect, the correct answer is: " + str(correct_answer_mult) + ".")
+if answer == correct_answer_div:    
+    print ("You are correct!")
+else:    
+    print ("This is incorrect, the correct answer is: " + str(correct_answer_div) + ".")        
+#Showing the correct answers
     
